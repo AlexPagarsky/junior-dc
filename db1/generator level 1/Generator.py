467a94115d313fcaf7d2b7da3392ed4dc41ad7d2
AlexPagarsky <alex.pagarski@gmail.com>
2020-04-18 19:36:20 +0300

    generator polishing

--- db1/generator level 1/Generator.py
+++ db1/generator level 1/Generator.py
@@ -19,38 +19,64 @@ def generator(file: str, timestamps: bool = False, multiline: bool = False) -> L
             for var_name, data_type in fields.items():
                 statement.extend((indent, name.lower(), '_' + var_name, ' ', data_type.upper(), ',', separator))
 
-            # TODO: Merge two if's, stupid sooqa
             if timestamps:
                 statement.extend(
-                    (indent, name.lower(), '_created TIMESTAMP DEFAULT current_timestamp', ',', separator,
-                     indent, name.lower(), '_updated TIMESTAMP DEFAULT current_timestamp', separator, ',')
+                    (
+                        indent, name.lower(), '_created TIMESTAMP DEFAULT current_timestamp', ',', separator,
+                        indent, name.lower(), '_updated TIMESTAMP DEFAULT current_timestamp', separator, ','
+                    )
                 )
-                statements.append(
-                    ''.join(
-                        (
-                            'CREATE FUNCTION update_date() RETURNS TRIGGER', separator,
-                            'LANGUAGE plpgsql',separator,
-                            'AS $update$', separator,
-                            'BEGIN', separator,
-                            indent, 'NEW.' + name.lower() + '_updated := current_timestamp;', separator,
-                            indent, 'RETURN NEW;', separator,
-                            'END;', separator,
-                            '$update$;'
+                statements.extend(
+                    (
+                        ''.join(
+                            (
+                                'CREATE FUNCTION update_date() RETURNS TRIGGER', separator,
+                                'LANGUAGE plpgsql',separator,
+                                'AS $update$', separator,
+                                'BEGIN', separator,
+                                indent, 'NEW.' + name.lower() + '_updated := current_timestamp;', separator,
+                                indent, 'RETURN NEW;', separator,
+                                'END;', separator,
+                                '$update$;'
+                            )
+                        ),
+                        ''.join(
+                            (
+                                'CREATE FUNCTION create_date() RETURNS TRIGGER', separator,
+                                'LANGUAGE plpgsql',separator,
+                                'AS $update$', separator,
+                                'BEGIN', separator,
+                                indent, 'NEW.' + name.lower() + '_created := current_timestamp;', separator,
+                                indent, 'RETURN NEW;', separator,
+                                'END;', separator,
+                                '$update$;'
+                            )
                         )
                     )
                 )
-
-                statements.append(
-                    ''.join(
-                        ('CREATE TRIGGER update_time AFTER UPDATE ON ', name, separator,
-                         'FOR EACH STATEMENT', separator,
-                         'EXECUTE PROCEDURE update_date();', separator)
+                statements.extend(
+                    (
+                        ''.join(
+                            (
+                                'CREATE TRIGGER update_time AFTER UPDATE ON ', name, separator,
+                                'FOR EACH STATEMENT', separator,
+                                'EXECUTE PROCEDURE update_date();', separator
+                            )
+                        ),
+                        ''.join(
+                            (
+                                'CREATE TRIGGER create_time AFTER INSERT ON ', name, separator,
+                                'FOR EACH STATEMENT', separator,
+                                'EXECUTE PROCEDURE create_date();', separator
+                            )
+                        )
                     )
                 )
             statement[-1] = ');'
             statements.insert(-2, ''.join(statement))
 
             # TODO: Finish second trigger (?)
+            # TODO: SET PRICE NOT NULL IN STATEMENTS, DURILA
 
     return statements
 
